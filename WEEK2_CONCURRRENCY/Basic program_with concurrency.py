b910034cbf7ff1f96294955ee1e4e25b8eb44cc2
Tanatswanashe Mthembu <kimblemthembu2002@gmail.com>
2025-08-12 14:47:47 +0200

    Update Basic program_with concurrency.py
    changedto sleep(0.2) in line 36

--- WEEK2_CONCURRRENCY/Basic program_with concurrency.py
+++ WEEK2_CONCURRRENCY/Basic program_with concurrency.py
@@ -1,5 +1,6 @@
 #import threading modules
 import threading
+from time import sleep
 
 #Make the classes subclasses of 'Thread'
 class Hello(threading.Thread):
@@ -10,6 +11,7 @@ class Hello(threading.Thread):
     def run(self):
         for i in range(self.num):
             print("Hello")
+            sleep(1)
 
 class Hi(threading.Thread):
     def __init__(self,num):
@@ -20,6 +22,7 @@ class Hi(threading.Thread):
     def run(self):
         for i in range(self.num):
             print("Hi")
+            sleep(1)
 
 
 def main():
@@ -30,7 +33,11 @@ def main():
     obj2 = Hi(num)
 
     obj1.start()
+    sleep(0.2) # aids in avoidng collisions
     obj2.start()
+
+    obj1.join()
+    obj2.join()     # for collsions if there is any code running in main thread
 #use member function of 'Thread' class '.start()'
 #this function takes a thread from 'initialized' state to 'running' state
 #when you subclass the classes that is when it has only been initilized meaning that the thtread object exists but isn't running
@@ -39,10 +46,16 @@ def main():
 """ This is because the theading constructor should be called instead of your normal constructor:
     'def __init__(self, exec_target, exec_args):
         threading.Thread.__init__(self, target=exec_target, args=exec_args)
+        exec_target is the function tat the thread must be created for 
+        exec_args: is the arguments of that function
 """
 
  
 
 main()
+
+
+
+
 
 
